721e6b44288387dafc59b4405927111fcb85eb1f
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-10-27 22:00:57 +0800

    remove uart code

--- main.c
+++ main.c
@@ -1,23 +1,3 @@
-#define UART_BASE	0x1fe001e0
-#define UART_RX		0   /* In:  Receive buffer */
-#define UART_TX		0   /* Out: Transmit buffer */
-#define UART_LSR	5   /* In:  Line Status Register */
-#define UART_LSR_TEMT	0x40 /* Transmitter empty */
-#define UART_LSR_THRE	0x20 /* Transmit-hold-register empty */
-#define UART_LSR_DR	0x01 /* Receiver data ready */
-
-void uart_put_c(char c) {
-	while (!(*((volatile char*)UART_BASE + UART_LSR) & (UART_LSR_THRE)));
-	*((volatile char*)UART_BASE + UART_TX) = c;
-}
-
-void print_s(const char *c) {
-	while (*c) {
-		uart_put_c(*c);
-		c ++;
-	}
-}
-
 #define IO_SIMU_ADDR            0xbfafff00
 #define UART_ADDR               0xbfafff10
 #define SIMU_FLAG_ADDR          0xbfafff20
@@ -44,7 +24,6 @@ void print_s(const char *c) {
 
 int main()
 {
-	// print_s("\nHere is my first bare-metal machine program on LoongArch32!\n\n");
 	SOC_LED = 0xaaaa;
 	SOC_LED_RG0 = 0x2;
 	SOC_LED_RG1 = 0x2;
