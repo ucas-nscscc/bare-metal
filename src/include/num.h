aceb2d91b8ed42301c889ac1f97424001c3f60e0
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-04 18:10:22 +0800

    all indent use tabs, add white line at the end of files

--- src/include/num.h
+++ src/include/num.h
@@ -2,10 +2,10 @@
 #define _NUM_H
 
 struct num {
-        void *addr;
-        unsigned cur_value;
-        void (*set_one)(struct num *, unsigned int, unsigned char);
-        void (*set_all)(struct num *, unsigned int);
+	void *addr;
+	unsigned cur_value;
+	void (*set_one)(struct num *, unsigned int, unsigned char);
+	void (*set_all)(struct num *, unsigned int);
 };
 
 #endif
