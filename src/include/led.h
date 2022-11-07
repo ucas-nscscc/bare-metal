fadbebdfe520316405235d498570cf783d4f7ccc
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-07 09:54:14 +0800

    set indent and use base addr + offset to present address

--- src/include/led.h
+++ src/include/led.h
@@ -2,19 +2,18 @@
 #define _LED_H
 
 struct led {
-        void *addr;
-        unsigned short status;
-        void (*turn_on_num)(struct led *, unsigned int);
-        void (*turn_off_num)(struct led *, unsigned int);
-
-        void (*turn_on_mask)(struct led *, unsigned short);
-        void (*turn_off_mask)(struct led *, unsigned short);
+	void *addr;
+	unsigned short status;
+	void (*turn_on_num)(struct led *, unsigned int);
+	void (*turn_off_num)(struct led *, unsigned int);
+	void (*turn_on_mask)(struct led *, unsigned short);
+	void (*turn_off_mask)(struct led *, unsigned short);
 };
 
 struct led_rg {
-        void *addr;
-        void (*turn_on)(struct led_rg *, unsigned int color);
-        void (*turn_off)(struct led_rg *);
+	void *addr;
+	void (*turn_on)(struct led_rg *, unsigned int color);
+	void (*turn_off)(struct led_rg *);
 };
 
 #endif
--- src/include/mysoc.h
+++ src/include/mysoc.h
@@ -4,20 +4,36 @@
 #include <soc.h>
 #include <timer.h>
 
-#define IO_SIMU_ADDR		0xbfafff00
-#define UART_ADDR		0xbfafff10
-#define SIMU_FLAG_ADDR		0xbfafff20
-#define OPEN_TRACE_ADDR		0xbfafff30
-#define NUM_MONITOR_ADDR	0xbfafff40
-#define LED_ADDR		0xbfaff020
-#define LED_RG0_ADDR		0xbfaff030
-#define LED_RG1_ADDR		0xbfaff040
-#define NUM_ADDR		0xbfaff050
-#define SWITCH_ADDR		0xbfaff060
-#define BTN_KEY_ADDR		0xbfaff070
-#define BTN_STEP_ADDR		0xbfaff080
-#define SW_INTER_ADDR		0xbfaff090 //switch interleave
-#define TIMER_ADDR		0xbfafe000
+#define GPIO_BASE               0xbfaf0000
+#define IO_SIMU_OFFSET		0xff00
+#define UART_OFFSET		0xff10
+#define SIMU_FLAG_OFFSET	0xff20
+#define OPEN_TRACE_OFFSET	0xff30
+#define NUM_MONITOR_OFFSET	0xff40
+#define LED_OFFSET		0xf020
+#define LED_RG0_OFFSET		0xf030
+#define LED_RG1_OFFSET		0xf040
+#define NUM_OFFSET		0xf050
+#define SWITCH_OFFSET		0xf060
+#define BTN_KEY_OFFSET		0xf070
+#define BTN_STEP_OFFSET		0xf080
+#define SW_INTER_OFFSET		0xf090 //switch interleave
+#define TIMER_OFFSET		0xe000
+
+#define IO_SIMU_ADDR		(GPIO_BASE + IO_SIMU_OFFSET)
+#define UART_ADDR		(GPIO_BASE + UART_OFFSET)
+#define SIMU_FLAG_ADDR		(GPIO_BASE + SIMU_FLAG_OFFSET)
+#define OPEN_TRACE_ADDR		(GPIO_BASE + OPEN_TRACE_OFFSET)
+#define NUM_MONITOR_ADDR	(GPIO_BASE + NUM_MONITOR_OFFSET)
+#define LED_ADDR		(GPIO_BASE + LED_OFFSET)
+#define LED_RG0_ADDR		(GPIO_BASE + LED_RG0_OFFSET)
+#define LED_RG1_ADDR		(GPIO_BASE + LED_RG1_OFFSET)
+#define NUM_ADDR		(GPIO_BASE + NUM_OFFSET)
+#define SWITCH_ADDR		(GPIO_BASE + SWITCH_OFFSET)
+#define BTN_KEY_ADDR		(GPIO_BASE + BTN_KEY_OFFSET)
+#define BTN_STEP_ADDR		(GPIO_BASE + BTN_STEP_OFFSET)
+#define SW_INTER_ADDR		(GPIO_BASE + SW_INTER_OFFSET) //switch interleave
+#define TIMER_ADDR		(GPIO_BASE + TIMER_OFFSET)
 
 #define SOC_LED		(*(volatile unsigned *)LED_ADDR     )
 #define SOC_LED_RG0	(*(volatile unsigned *)LED_RG0_ADDR )
@@ -29,9 +45,9 @@
 #define SOC_TIMER	(*(volatile unsigned *)TIMER_ADDR   )
 
 #define MYSOC_TIMER_FREQ	(100 MHz)
-#define LED_RG_GREEN		0x1
-#define LED_RG_RED		0x2
-#define LED_RG_GREEN_RED 	0x3
+#define LED_RG_GREEN		(0x1)
+#define LED_RG_RED		(0x2)
+#define LED_RG_GREEN_RED 	(LED_RG_GREEN | LED_RG_RED)
 
 struct soc *mysoc_init();
 
