a5a945b831301eee17a0ced1e9878bced38d93b8
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-12-03 00:46:09 +0800

    yield when task blocked

--- src/libs/funcs.c
+++ src/libs/funcs.c
@@ -15,9 +15,8 @@ int horse_race_lamp(void *arg)
 		gpio_write(led, led_status);
 		time = get_sec();
 
-		while (get_sec() - time < 1) {
+		while (get_sec() - time < 1)
 			yield();
-		}
 		
 		i = (i + 1) % 16;
 		led_status = (led_status << 1) | ((led_status & 0x8000) >> 15);
@@ -35,7 +34,8 @@ int fib(void *arg)
 	int sw = gpio_open("sw", "r");
 
 	while (1) {
-		// while (((gpio_read(btn_key) >> 15) & 0x1) == 0) ;
+		while (((gpio_read(btn_key) >> 15) & 0x1) == 0)
+			yield();
 		unsigned char end_i = ~gpio_read(sw);
 		
 		unsigned int f0 = 1, f1 = 1, f2, sum;
@@ -54,5 +54,9 @@ int fib(void *arg)
 		gpio_write(num, sum);
 		yield();
 	}
+
+	gpio_close(num);
+	gpio_close(btn_key);
+	gpio_close(sw);
 	return 0;
 }
