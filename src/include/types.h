5734b54d62d04da57e8eee4ee461fd4255927ab3
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-12-07 22:34:10 +0800

    change size_t to unsigned long

--- src/include/types.h
+++ src/include/types.h
@@ -9,7 +9,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
-typedef unsigned int size_t;
+typedef unsigned long size_t;
 
 #define NULL ((void *)0)
 
